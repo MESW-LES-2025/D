1d37cb656ba538b8ba2a4cf6fee4af8f07e334dd
Gabriel Dias <gabriel.h.dias99@hotmail.com>
2025-11-09 16:34:41 -0300

    refactor: update home page to redirect based on auth status

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,21 @@
 import type { Metadata } from 'next';
-import { CounterForm } from '@/components/CounterForm';
-import { CurrentCount } from '@/components/CurrentCount';
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
+import { auth } from '@/lib/auth/auth';
 
 export const metadata: Metadata = {
   title: 'TaskUp',
   description: 'A simple task management app to boost your productivity.',
 };
 
-export default function Home() {
-  return (
-    <div className="min-h-screen bg-gray-50 py-12">
-      <div className="mx-auto max-w-lg px-6">
-        <div className="rounded-lg bg-white p-8 shadow-md">
-          <h1 className="mb-8 text-center text-3xl font-bold text-gray-800">
-            Setup App
-          </h1>
-
-          <div className="space-y-6">
-            <div className="text-center">
-            </div>
-
-            <CurrentCount />
-            <CounterForm />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+export default async function Home() {
+  // Ensure the user is authenticated
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (session) {
+    redirect('/dashboard');
+  } else {
+    redirect('/sign-in');
+  }
 }
