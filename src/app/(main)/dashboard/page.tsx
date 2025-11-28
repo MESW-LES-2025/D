d1bdcf806f9b7ee538b90d9641f72ebb38e59eeb
Gabriel Dias <gabriel.h.dias99@hotmail.com>
2025-11-28 16:46:37 -0300

    feat: added dashboard page

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,16 +1,89 @@
 import type { Metadata } from 'next';
+import { and, asc, eq, gte, inArray, lt } from 'drizzle-orm';
 import { headers } from 'next/headers';
 import { redirect } from 'next/navigation';
-
-import { InviteOrgDialog } from '@/components/dialogs/invite-org-dialog';
+import { Dashboard } from '@/components/dashboard/Dashboard';
 import { NoOrganization } from '@/components/empty/no-organization';
-import { Button } from '@/components/ui/button';
 import { auth } from '@/lib/auth/auth';
+import { db } from '@/lib/db';
+import { taskAssigneesTable, taskTable, userTable } from '@/schema';
 
 export const metadata: Metadata = {
   title: 'TaskUp | Dashboard',
   description: 'Your personal dashboard to manage tasks efficiently.',
 };
+
+async function fetchMonthlyDoneTasks(organizationId: string, firstDay: Date, lastDay: Date) {
+  try {
+    return await db
+      .select({
+        id: taskTable.id,
+        dueDate: taskTable.dueDate,
+        difficulty: taskTable.difficulty,
+        status: taskTable.status,
+        score: taskTable.score,
+      })
+      .from(taskTable)
+      .where(
+        and(
+          eq(taskTable.organizationId, organizationId ?? ''),
+          eq(taskTable.status, 'done'),
+          gte(taskTable.dueDate, firstDay),
+          lt(taskTable.dueDate, lastDay),
+        ),
+      )
+      .orderBy(asc(taskTable.dueDate));
+  } catch (error) {
+    console.error('Error fetching tasks:', error);
+    return [];
+  }
+}
+
+async function fetchAssigneesByTaskIs(taskIds: string[]) {
+  try {
+    return await db
+      .select({
+        taskId: taskAssigneesTable.taskId,
+        user: {
+          id: userTable.id,
+          name: userTable.name,
+          image: userTable.image,
+        },
+      })
+      .from(taskAssigneesTable)
+      .innerJoin(userTable, eq(taskAssigneesTable.userId, userTable.id))
+      .where(inArray(taskAssigneesTable.taskId, taskIds));
+  } catch (error) {
+    console.error('Error fetching assignees:', error);
+    return [];
+  }
+}
+
+async function fetchUserMonthlyTasks(userId: string, organizationId: string, firstDay: Date, lastDay: Date) {
+  try {
+    return await db
+      .select({
+        id: taskTable.id,
+        dueDate: taskTable.dueDate,
+        difficulty: taskTable.difficulty,
+        status: taskTable.status,
+        score: taskTable.score,
+      })
+      .from(taskTable)
+      .innerJoin(taskAssigneesTable, eq(taskAssigneesTable.taskId, taskTable.id))
+      .where(
+        and(
+          eq(taskTable.organizationId, organizationId),
+          eq(taskAssigneesTable.userId, userId),
+          gte(taskTable.dueDate, firstDay),
+          lt(taskTable.dueDate, lastDay),
+        ),
+      );
+  } catch (error) {
+    console.error('Error fetching users tasks:', error);
+    return [];
+  }
+}
 
 export default async function Home() {
   const session = await auth.api.getSession({
@@ -24,15 +97,42 @@ export default async function Home() {
     return <NoOrganization />;
   }
 
-  return (
-    <div className="py-12">
-      <div className="flex max-w-lg flex-col">
-        Welcome to the dashboard
+  const today = new Date();
+  const firstDay = new Date(today.getFullYear(), today.getMonth(), 1);
+  const lastDay = new Date(today.getFullYear(), today.getMonth() + 1, 1);
 
-        <InviteOrgDialog>
-          <Button variant="outline">Invite Member</Button>
-        </InviteOrgDialog>
+  // Fetch tasks from the current month with status equal 'done'
+  const monthlyDoneTasks = await fetchMonthlyDoneTasks(session.session?.activeOrganizationId, firstDay, lastDay);
+
+  // Fetch assignees from the tasks
+  const assignees = monthlyDoneTasks.length > 0
+    ? await fetchAssigneesByTaskIs(monthlyDoneTasks.map(t => t.id))
+    : [];
+
+  // Fetch all tasks from the current month and from the current user
+  const currentUsermonthlyTasks = monthlyDoneTasks.length > 0
+    ? await fetchUserMonthlyTasks(session.session?.userId, session.session?.activeOrganizationId, firstDay, lastDay)
+    : [];
+
+  return (
+    <div className="h-full flex-1 flex-col gap-8 px-8 py-4 md:flex">
+      <div className="flex items-center justify-between gap-2">
+        <div className="flex flex-col gap-1">
+          <h2 className="text-2xl font-semibold tracking-tight">
+            Dashboard
+          </h2>
+          <p className="text-muted-foreground">
+            Manage your tasks effectively with our intuitive task tracker.
+          </p>
+        </div>
       </div>
+      <Dashboard
+        monthlyDoneTasks={monthlyDoneTasks}
+        assignees={assignees}
+        currentUsermonthlyTasks={currentUsermonthlyTasks}
+        currentUserId={session.session?.userId}
+        serverDate={today} // This avoid errors if the user is in a different timezone
+      />
     </div>
   );
 }
---
+++ src/components/dashboard/ComparisonCard.tsx
@@ -0,0 +1,118 @@
+import { TrendingDown, TrendingUp } from 'lucide-react';
+import { cn } from '@/lib/utils';
+import { Card, CardContent, CardHeader, CardTitle } from '../ui/card';
+
+type ComparisonCardProps = {
+  currentUserPoints: number;
+  users: UserStats[];
+};
+
+type UserStats = {
+  image?: string | null;
+  name: string | null;
+  taskScore: number;
+  taskCount: number;
+};
+
+function warning(currentUserAboveTemAverage: boolean, currentUserPoints: number, teamAveragePoints: number) {
+  if (currentUserPoints === 0 && teamAveragePoints === 0) {
+    return '';
+  }
+
+  const percent = ((currentUserPoints - teamAveragePoints) / teamAveragePoints) * 100;
+  const percentFormated = Math.abs(percent).toFixed(1).toLocaleString();
+
+  if (currentUserAboveTemAverage) {
+    return (
+      <div className="rounded-lg border border-green-500/20 bg-green-500/10 p-4">
+        <div className="flex items-start gap-2">
+          <TrendingUp className="mt-0.5 h-5 w-5 flex-shrink-0 text-green-500" />
+          <div>
+            <p className="mb-1">Outstanding Performance!</p>
+            <p className="text-sm text-green-400">
+              You're
+              {' '}
+              {percentFormated}
+              % above team average
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="rounded-lg border border-red-500/20 bg-red-500/10 p-4">
+      <div className="flex items-start gap-2">
+        <TrendingDown className="mt-0.5 h-5 w-5 flex-shrink-0 text-red-500" />
+        <div>
+          <p className="mb-1">Keep Going — You’ve Got This!</p>
+          <p className="text-sm text-red-400">
+            { currentUserPoints === 0
+              ? 'No points yet, but great things begin from zero!'
+              : `You’re trending ${percentFormated} % below the team average right now.`}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function ComparisonCard({ currentUserPoints, users }: ComparisonCardProps) {
+  const teamTotalScore = users.reduce((sum, user) => sum + user.taskScore, 0);
+
+  const teamAveragePoints = users.length > 0
+    ? teamTotalScore / users.length
+    : 0;
+
+  const currentUserAboveTemAverage = currentUserPoints >= teamAveragePoints;
+
+  return (
+    <Card className={cn('w-full py-4 gap-0')}>
+      <CardHeader className="gap-0">
+        <CardTitle className="mb-8 text-sm text-gray-400">
+          You vs. Team
+        </CardTitle>
+      </CardHeader>
+      <CardContent className="space-y-8 py-4">
+        <div>
+          <div className="mb-2 flex items-center justify-between">
+            <span className="text-gray-400">You</span>
+            <span className="font-bold">
+              {currentUserPoints.toLocaleString()}
+              {' '}
+              points
+            </span>
+          </div>
+          <div className="h-3 overflow-hidden rounded-full bg-muted">
+            <div
+              className="h-full rounded-full bg-muted-foreground transition-all duration-1000"
+              style={{ width: currentUserAboveTemAverage ? '100%' : `${(currentUserPoints / teamAveragePoints) * 100}%` }}
+            />
+          </div>
+        </div>
+
+        {/* Team average */}
+        <div>
+          <div className="mb-2 flex items-center justify-between">
+            <span className="text-gray-400">Team Average</span>
+            <span className="font-bold">
+              {teamAveragePoints.toFixed(1).toLocaleString()}
+              {' '}
+              points
+            </span>
+          </div>
+          <div className="h-3 overflow-hidden rounded-full bg-muted">
+            <div
+              className="h-full rounded-full bg-primary transition-all duration-1000"
+              style={{ width: !currentUserAboveTemAverage ? '100%' : `${(teamAveragePoints / currentUserPoints) * 100}%` }}
+            />
+          </div>
+        </div>
+
+        {warning(currentUserAboveTemAverage, currentUserPoints, teamAveragePoints)}
+
+      </CardContent>
+    </Card>
+  );
+}
