072e14755a1cb9cd8723e8a16e36621e2643420f
Eduardo Miranda <eduardomiranda_pf@hotmail.com>
2025-11-28 15:17:27 +0000

    Fixed migration errors

--- migrations/0008_black_alex_wilder.sql
+++
@@ -1,2 +0,0 @@
-DROP TABLE IF EXISTS "counter" CASCADE;--> statement-breakpoint
-ALTER TABLE "tasks" ADD COLUMN "created_at" timestamp DEFAULT now() NOT NULL;
--- migrations/0009_dashing_manta.sql
+++
@@ -1,2 +0,0 @@
-DROP TABLE "task_labels" CASCADE;--> statement-breakpoint
-DROP TYPE "public"."task_label_color";
